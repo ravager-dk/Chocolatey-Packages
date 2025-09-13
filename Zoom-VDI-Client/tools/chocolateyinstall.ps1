b2e4a68c619a93f6b066b7898b40f4b1ad9c74cd
Martin Nygaard Jensen <martin@ravager.dk>
2025-09-13 23:00:48 +0200

    Bump Zoom VDI Client and Universal Plugin versions to 6.5.10.26710, update download URLs and checksums
    Signed-off-by: Martin Nygaard Jensen <martin@ravager.dk>

--- Zoom-VDI-Client/tools/chocolateyinstall.ps1
+++ Zoom-VDI-Client/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿
 $ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url      = "https://zoom.us/download/vdi/6.4.12.26620/ZoomInstallerVDI.msi?archType=x64"
+$url      = "https://zoom.us/download/vdi/6.5.10.26710/ZoomInstallerVDI.msi?archType=x64"
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
@@ -10,7 +10,7 @@ $packageArgs = @{
   url64          = $url
   softwareName   = 'Zoom Client for VDI*'
 
-  checksum64     = 'DFB2D13C9DB375D8629ABD6C2C1EC5DC39E94ED0F29C4D8266A0B6EF7FA25890'
+  checksum64     = '2591524E20C45E373A44728D3C21D7E13D3F84BC611926826D981CD955B401F2'
   checksumType64 = 'sha256'
 
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" DISABLEAUS=TRUE"
