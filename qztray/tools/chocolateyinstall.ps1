de78fb1e8ca18fb2aadc11b34a4dffc1ce09b4f4
Martin Nygaard Jensen <martin@ravager.dk>
2025-11-27 08:39:25 +0000

    Update QZ Tray to use available Windows binaries (arm64 and x86_64)

--- qztray/tools/chocolateyinstall.ps1
+++ qztray/tools/chocolateyinstall.ps1
@@ -3,11 +3,11 @@ $ErrorActionPreference = 'Stop'
 $packageArgs = @{
     PackageName    = 'qztray'
     FileType       = 'exe'
-    Url            = 'https://github.com/qzind/tray/releases/download/v2.2.5/qz-tray-2.2.5-x86.exe'
+    Url            = 'https://github.com/qzind/tray/releases/download/v2.2.5/qz-tray-2.2.5-arm64.exe'
     Url64          = 'https://github.com/qzind/tray/releases/download/v2.2.5/qz-tray-2.2.5-x86_64.exe'
+    ChecksumType   = 'sha256'
     Checksum       = ''
     Checksum64     = ''
-    ChecksumType   = 'sha256'
     SilentArgs     = '/S'
     ValidExitCodes = @(0)
 }
