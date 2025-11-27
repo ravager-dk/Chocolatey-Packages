af4a7ed3868a55b8488b876436fc506d4b82f9dc
Martin Nygaard Jensen <martin@ravager.dk>
2025-11-27 12:37:16 +0000

    Update curator-cli to use source zip with verified checksum

--- curator-cli/tools/chocolateyinstall.ps1
+++ curator-cli/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@ $ErrorActionPreference = 'Stop'
 $packageArgs = @{
     PackageName    = 'curator-cli'
     FileType       = 'zip'
-    Url            = 'https://github.com/elastic/curator/releases/download/v9.0.0/curator-9.0.0-windows.zip'
+    Url            = 'https://github.com/elastic/curator/archive/refs/tags/v9.0.0.zip'
     ChecksumType   = 'sha256'
-    Checksum       = ''
+    Checksum       = '5cfecfa4b779df27c6887044c8b4a5d864e957689dc60803a8ad1e37196cfad1'
     UnzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 }
 
