625b79068a778619ee4cddc2bd21622c5c03a3d8
Martin Nygaard Jensen <martin@ravager.dk>
2025-11-27 08:49:55 +0000

    Remove duplicate Url64, support only x86_64 for Chocolatey compatibility

--- curator-cli/tools/chocolateyinstall.ps1
+++ curator-cli/tools/chocolateyinstall.ps1
@@ -4,10 +4,8 @@ $packageArgs = @{
     PackageName    = 'curator-cli'
     FileType       = 'zip'
     Url            = 'https://github.com/elastic/curator/releases/download/v9.0.0/curator-9.0.0-windows.zip'
-    Url64          = 'https://github.com/elastic/curator/releases/download/v9.0.0/curator-9.0.0-windows.zip'
     ChecksumType   = 'sha256'
     Checksum       = ''
-    Checksum64     = ''
     UnzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 }
 
