ae6a06950637064b9cbcc7e09db23a846db24377
Martin Nygaard Jensen <martin@ravager.dk>
2025-11-27 08:49:34 +0000

    Remove ARM architecture, support only x86_64 for Chocolatey compatibility

--- qztray/tools/chocolateyinstall.ps1
+++ qztray/tools/chocolateyinstall.ps1
@@ -3,11 +3,9 @@ $ErrorActionPreference = 'Stop'
 $packageArgs = @{
     PackageName    = 'qztray'
     FileType       = 'exe'
-    Url            = 'https://github.com/qzind/tray/releases/download/v2.2.5/qz-tray-2.2.5-arm64.exe'
-    Url64          = 'https://github.com/qzind/tray/releases/download/v2.2.5/qz-tray-2.2.5-x86_64.exe'
+    Url            = 'https://github.com/qzind/tray/releases/download/v2.2.5/qz-tray-2.2.5-x86_64.exe'
     ChecksumType   = 'sha256'
     Checksum       = ''
-    Checksum64     = ''
     SilentArgs     = '/S'
     ValidExitCodes = @(0)
 }
