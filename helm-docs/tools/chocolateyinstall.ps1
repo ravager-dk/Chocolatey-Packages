da289b374b0b0b367fc7c74774256fe4f2a8e56d
Martin Nygaard Jensen <martin@ravager.dk>
2025-11-27 08:37:33 +0000

    Add correct download URLs and SHA256 checksums for helm-docs

--- helm-docs/tools/chocolateyinstall.ps1
+++ helm-docs/tools/chocolateyinstall.ps1
@@ -2,11 +2,11 @@ $ErrorActionPreference = 'Stop'
 
 $packageArgs = @{
     PackageName    = 'helm-docs'
-    Url            = 'https://github.com/norwoodj/helm-docs/releases/download/v1.14.2/helm-docs_1.14.2_windows_386.tar.gz'
-    Url64          = 'https://github.com/norwoodj/helm-docs/releases/download/v1.14.2/helm-docs_1.14.2_windows_amd64.tar.gz'
+    Url            = 'https://github.com/norwoodj/helm-docs/releases/download/v1.14.2/helm-docs_1.14.2_Windows_arm64.tar.gz'
+    Url64          = 'https://github.com/norwoodj/helm-docs/releases/download/v1.14.2/helm-docs_1.14.2_Windows_x86_64.tar.gz'
     ChecksumType   = 'sha256'
-    Checksum       = ''
-    Checksum64     = ''
+    Checksum       = 'c6a5f43e89837bd29749298ca27ed350ca8ee6ef64e25ce9653e709171dbbaa4'
+    Checksum64     = '3c54fd78d99e2769cf83a0faf12cf6cd4de1cac6ed7bee8d908a2c8fc23f538c'
     UnzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 }
 
