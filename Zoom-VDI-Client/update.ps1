3f06245e36b2d9899eca15d5fca1a5c6a0e29841
Martin Nygaard Jensen <martin@ravager.dk>
2026-02-02 20:20:22 +0100

    feat: Add Zoom VDI auto-update workflow and scripts

---
+++ Zoom-VDI-Client/update.ps1
@@ -0,0 +1,38 @@
+import-module au
+
+$releases = 'https://support.zoom.com/hc/en/article?id=zm_kb&sysparm_article=KB0063810'
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyinstall.ps1" = @{
+            "(?i)(^\s*`$url\s*=\s*).*" = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*`$checksum64\s*=\s*).*" = "`$1'$($Latest.Checksum32)'"
+        }
+        ".\zoom-vdi-client.nuspec" = @{
+            "(?i)(<version>).*(</version>)" = "`$1$($Latest.Version)`$2"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    # Use a user agent to avoid being blocked
+    $content = (Invoke-WebRequest -Uri $releases -UserAgent 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36' -UseBasicParsing).Content
+    
+    # Regex to find the VDI Client URL
+    # Looking for: https://zoom.us/download/vdi/{version}/ZoomInstallerVDI.msi?archType=x64
+    $urlMatch = [regex]::match($content, 'https://zoom.us/download/vdi/([\d\.]+)/ZoomInstallerVDI\.msi\?archType=x64')
+    
+    if ($urlMatch.Success) {
+        $version = $urlMatch.Groups[1].Value
+        $url = $urlMatch.Value
+        
+        return @{
+            Version = $version
+            URL32 = $url
+        }
+    } else {
+        throw "Could not find Zoom VDI Client download URL in the page."
+    }
+}
+
+Update-Package -ChecksumFor 32
---
+++ Zoom-VDI-Universal-Plugin/update.ps1
@@ -0,0 +1,39 @@
+import-module au
+
+$releases = 'https://support.zoom.com/hc/en/article?id=zm_kb&sysparm_article=KB0063810'
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyinstall.ps1"      = @{
+            "(?i)(^\s*`$url64\s*=\s*).*"      = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*`$checksum64\s*=\s*).*" = "`$1'$($Latest.Checksum32)'"
+        }
+        ".\zoom-vdi-universal-plugin.nuspec" = @{
+            "(?i)(<version>).*(</version>)" = "`$1$($Latest.Version)`$2"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    # Use a user agent to avoid being blocked
+    $content = (Invoke-WebRequest -Uri $releases -UserAgent 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36' -UseBasicParsing).Content
+    
+    # Regex to find the VDI Universal Plugin URL
+    # Looking for: https://zoom.us/download/vdi/{version}/ZoomVDIUniversalPluginx64.msi?archType=x64
+    $urlMatch = [regex]::match($content, 'https://zoom.us/download/vdi/([\d\.]+)/ZoomVDIUniversalPluginx64\.msi\?archType=x64')
+    
+    if ($urlMatch.Success) {
+        $version = $urlMatch.Groups[1].Value
+        $url = $urlMatch.Value
+        
+        return @{
+            Version = $version
+            URL32   = $url
+        }
+    }
+    else {
+        throw "Could not find Zoom VDI Universal Plugin download URL in the page."
+    }
+}
+
+Update-Package -ChecksumFor 32
